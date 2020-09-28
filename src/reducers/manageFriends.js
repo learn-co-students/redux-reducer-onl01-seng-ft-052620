2bddc0cfb22a4d641895e6e02982c96fa3df0e62
Luca <lucakanyo@protonmail.com>
2020-09-28 10:39:53 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,10 @@
 export function manageFriends(state, action){
+    switch (action.type){
+        case 'ADD FRIEND':
+            return {...state, friends: [...state.friends, action.friend]}
+        case 'REMOVE FRIEND':
+            return state 
+        default: 
+            return state
+    }
 }
