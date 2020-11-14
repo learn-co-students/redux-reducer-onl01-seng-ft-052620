010a1b2c6251a050a9555b159cf9ba3b99587a7c
chibuisi <enyia21@gmail.com>
2020-11-14 14:32:58 -0600

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,17 @@
 export function manageFriends(state, action){
+    // console.log(state.friends)
+    switch(action.type){
+        case 'ADD_FRIEND':
+        // if action.friend
+        let updatedState = Object.assign(
+            {},
+            state,
+            {
+                friends: state.friends
+            });
+        console.log(updatedState)
+        return updatedState;
+        default:
+            return state;
+    }
 }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,22 @@
 export function managePresents(state, action){
+    
+    switch(action.type){
+        case 'INCREASE':
+            // console.log("State = ", state);
+            // console.log("Action = ", action);
+            // let num = state.numberOfPresents + 1;
+            // let newState = {numberOfPresents: num};
+            // let numberOfPresents: {}
+            // numberOfPresents= {numberOfPresents + 1};
+            // let newState = {...state, numberOfPresents: state.numberOfPresents + 1}
+            let increasedState = Object.assign({}, state, {numberOfPresents: state.numberOfPresents + 1});
+            return increasedState;
+        case 'DECREASE':
+            let decreaseState = Object.assign({}, state, {numberOfPresents: state.numberOfPresents - 1});
+            // const numberOfPresents: {state.numberOfPresents - 1}
+            // return numberOfPresents;
+            return decreaseState;
+        default:
+            return state;
+    }
 }
